39a1d06714134b15ec9d3c1c2bbcc69bb12d8671
Preethu098 <preethunath@gmail.com>
2020-12-13 21:53:48 +0530

    4 files modified

--- src/app/authors.ts
+++ src/app/authors.ts
@@ -1,14 +1,17 @@
 export const authors =[
   {
     name: "Simon Joy",
-    description: "Simon's been a professional software developer for over 35 years. During his career, he has worked for major companies such as Fujitsu, Mitsubishi, and Saab.His video courses are used to train developers in major companies such as Mercedes-Benz, Paypal, VW, Pitney Bowes, IBM, and T-Mobile just to name a few (via the Udemy for Business program). What makes Simon unique is his professional programming career - many instructors have never programmed professionally, let alone had a distinguished professional development career like Simon. Simon has trained over 847,000 students how to program, way more than a typical IT Professor at a college does in a lifetime."
+    description:
+     "Simon's been a professional software developer for over 35 years. During his career, he has worked for major companies such as Fujitsu, Mitsubishi, and Saab.His video courses are used to train developers in major companies such as Mercedes-Benz, Paypal, VW, Pitney Bowes, IBM, and T-Mobile just to name a few (via the Udemy for Business program). What makes Simon unique is his professional programming career - many instructors have never programmed professionally, let alone had a distinguished professional development career like Simon. Simon has trained over 847,000 students how to program, way more than a typical IT Professor at a college does in a lifetime."
   },
   {
     name :"David Rose",
-    description: "David Rose is an author, instructor and speaker in the Java development world. David is a contributing author to several Java books, including  Professional Java E-Commerce (Wrox Press), Beginning Java Networking  (Wrox Press), and XML and Web Services Unleashed (Sams Publishing). David  has Java certifications from Oracle and IBM. He holds a B.S. in Computer Science from Boston University."
+    description: 
+    "David Rose is an author, instructor and speaker in the Java development world. David is a contributing author to several Java books, including  Professional Java E-Commerce (Wrox Press), Beginning Java Networking  (Wrox Press), and XML and Web Services Unleashed (Sams Publishing). David  has Java certifications from Oracle and IBM. He holds a B.S. in Computer Science from Boston University."
   },
   {
     name:"Emily Fields",
-    description: "Emily Fields is an experienced software engineer and software architect.In her professional career she has developed numerous flagship products and has worked on cutting-edge solutions and technologies for companies like Google, Intel and many others. She is primarily interested in real time and high scale software, and demonstrated her technical leadership skills working at many leading companies in their fields. She is also programming language and technology agnostic and her teaching methods emphasize understanding the core fundamentals, and developing the right way of thinking and good practices. Her passion for technology made her a good mentor for other engineers and students, and she is excited to share her knowledge and experience here at Udemy."
+    description: 
+    "Emily Fields is an experienced software engineer and software architect.In her professional career she has developed numerous flagship products and has worked on cutting-edge solutions and technologies for companies like Google, Intel and many others. She is primarily interested in real time and high scale software, and demonstrated her technical leadership skills working at many leading companies in their fields. She is also programming language and technology agnostic and her teaching methods emphasize understanding the core fundamentals, and developing the right way of thinking and good practices. Her passion for technology made her a good mentor for other engineers and students, and she is excited to share her knowledge and experience here at Udemy."
   }
 ]
