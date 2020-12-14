43111f3bf9503b1c39d3f2b694f880cb58a36b96
Preethu098 <preethunath@gmail.com>
2020-12-14 19:07:16 +0530

    2 files modified

--- src/app/courses.ts
+++ src/app/courses.ts
@@ -3,6 +3,7 @@ export const courses = [
     name: 'Java For Beginners',
     description: 'Learn Java In This Course And Become a Computer Programmer. Obtain valuable Core Java Skills And Java Certification',
     author : 'Simon Joy',
+    authorId : 0,
     rating : 4.5,
     duration : '40 hours, 212 Lectures, All Levels' ,
     price: 1500
@@ -12,6 +13,7 @@ export const courses = [
     name: 'Python Bootcamp : Zero to Mastery',
     description: 'Learn Python like a Professional Start from the basics and go all the way to creating your own applications and games',
     author : 'David Rose',
+    authorId : 1,
     rating : 4.4,
     duration : '30 hours, 150 Lectures, All Levels' ,
     price: 2000
@@ -21,6 +23,7 @@ export const courses = [
     name: 'Learn Spring Boot - Rapid Spring Application Development',
     description: 'Spring Boot gives you all the power of the Spring Framework without all of the complexity. Start writing apps today!',
     author : 'Emily Fields',
+    authorId : 2,
     rating : 4.5,
     duration : '45 hours, 250 Lectures, All Levels' ,
     price :2500
